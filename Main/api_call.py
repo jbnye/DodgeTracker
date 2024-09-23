380c3741fc3aec8b155ebbd474d8199ff42fbb61
Jbnye <jbnye@umich.edu>
2024-09-22 23:55:51 -0400

    added new apis and set up the loop to get all challenger players. Also saved api key in a .env file.

--- Main/api_call.py
+++ Main/api_call.py
@@ -1,20 +1,69 @@
+# pretty print
 import pprint
+#requests for api
 import requests
+#honestly no clue, i think its the objects for the enviroment
+import os
+#needed for hidden api
+from dotenv import load_dotenv
 
-api_key = "RGAPI-ee366c81-c961-4eb2-a916-a77c2cb04561"
-#api_key =  "RGAPI-8e66f881-a52e-4ac5-a222-4a91fad3015c"
+load_dotenv()
+#getting the api key from the .env
+api_key = os.getenv("Riot_Api_Key")
 
-url = "https://americas.api.riotgames.com/"
-id = "Flying%20Cow/NA1"
-account_path = f"riot/account/v1/accounts/by-riot-id/{id}?api_key={api_key}"
-print(url+account_path)
-response = requests.get(url+account_path)
-# print(response.json().get("puuid"))
-account_puuid = response.json().get("puuid")
+# url1 is new api url for riot, url2 is for the newer api calls
+url = "https://americas.api.riotgames.com"
+url2 = "https://na1.api.riotgames.com"
 
-matches_path = f"{url}lol/match/v5/matches/by-puuid/{account_puuid}/ids?type=ranked&start=0&count=1&api_key={api_key}"
-#print(matches_path)
-#print("https://americas.api.riotgames.com/lol/match/v5/matches/by-puuid/IVga1cf6XMrNUogvgZMghwMwzDXcquSA40azk0rNPTfKGJCq9O-4lskw2sww5XqYi--ntlIV0UANpQ/ids?start=0&count=20&api_key=RGAPI-ee366c81-c961-4eb2-a916-a77c2cb04561")
-response  = requests.get("https://americas.api.riotgames.com/lol/match/v5/matches/by-puuid/IVga1cf6XMrNUogvgZMghwMwzDXcquSA40azk0rNPTfKGJCq9O-4lskw2sww5XqYi--ntlIV0UANpQ/ids?start=0&count=1&api_key=RGAPI-ee366c81-c961-4eb2-a916-a77c2cb04561")
-print(response.json())
-lp_path = f""
+
+
+
+# function to get all challegner players
+def test(count):
+
+    getChallengers_path = f"/lol/league/v4/challengerleagues/by-queue/RANKED_SOLO_5x5?api_key= {api_key}"
+    challenger_players_response = requests.get(url2+getChallengers_path)
+    challenger_players = challenger_players_response.json()
+    for players in challenger_players['entries']:
+        count += 1
+    return count
+
+count = 0
+count = test(count)
+print(count)
+
+#test name
+name = "Flying%20Cow/NA1"
+
+#gets the account puuid
+account_path = f"/riot/account/v1/accounts/by-riot-id/{name}?api_key={api_key}"
+account_response = requests.get(url+account_path)
+account_puuid = account_response.json().get("puuid")
+print(account_puuid)
+
+
+#gets the last rnaked matchid they played for ranked
+matches_path = f"{url}/lol/match/v5/matches/by-puuid/{account_puuid}/ids?type=ranked&start=0&count=1&api_key={api_key}"
+matches_response = requests.get(matches_path)
+print(matches_response)
+print(matches_response.json())
+
+#gets the summoner ID used for profileicon and to get their accountID and id using their puuid
+summoner_path = f"{url2}/lol/summoner/v4/summoners/by-puuid/{account_puuid}?api_key={api_key}"
+summoner_response = requests.get(summoner_path)
+print(summoner_response)
+id = summoner_response.json().get("id")
+account_id = summoner_response.json().get("accountId")
+profile_icon = summoner_response.json().get("profileIconId")
+
+#gets the ranked data for a summoner, currently retrieving ranked solo queue, tier, rank, and league points
+rank_path = f"{url2}/lol/league/v4/entries/by-summoner/{id}?api_key={api_key}"
+rank_response = requests.get(rank_path)
+rank_response = rank_response.json()[0]
+print(rank_response)
+summoner_rank = rank_response.get("tier")
+summoner_div = rank_response.get("rank")
+summoner_lp = repr(rank_response.get("leaguePoints")) #converts to string
+print(summoner_rank +" " + summoner_div + " " + summoner_lp)
+
+
