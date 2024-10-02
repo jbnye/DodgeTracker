89c6519fef8d3d95f4253c2bb583f553e2999ccc
Jbnye <jbnye@umich.edu>
2024-10-02 18:43:44 -0400

    added some syntax fixes, also created a new file that im going to use called populate_summoners that will pre populate everyone masters+ and get their account info, but will need to do it slower

--- Main/populate_summoners.py
+++ Main/populate_summoners.py
@@ -11,24 +11,23 @@ from datetime import datetime
 api_key = os.getenv("Riot_Api_Key")
 
 
+conn = get_db_connection()
+cursor = conn.cursor()
+
+
+
 
 # function to check if the summoner exists. Search the summonerId in the database and fetch the results and send back to update or insert
 def check_summoner_exists(summoner_id):
 
-    conn = get_db_connection()
-    cursor = conn.cursor()
     check_query = "SELECT leaguePoints, gamesPlayed FROM Summoner WHERE summonerId = %s"
     cursor.execute(check_query, (summoner_id,))
     result = cursor.fetchone()
-    conn.close()
     return result
 
 
+def update_summoner_info(summoner_id):
 
-def update_summoner_info(summoner_id, league_points):
-
-    conn = get_db_connection()
-    cursor = conn.cursor()
 
     try:
         getSummoner_path = f"https://na1.api.riotgames.com/lol/summoner/v4/summoners/{summoner_id}?api_key={api_key}"
@@ -48,20 +47,10 @@ def update_summoner_info(summoner_id, league_points):
         account_game_name = account["gameName"]
         account_tagLine = account["tagLine"]
 
-        update_query = """
-            UPDATE Summoner
-            SET
-                leaguePoints = %s
-                iconId = %s,
-                summonerLevel = %s,
-                puuId = %s,
-                gameName = %s,
-                tagLine = %s
-            WHERE summonerId = %s    
-        """
-        data = (league_points, summoner_profile_icon,summoner_level, summoner_puuid, account_game_name, account_tagLine, summoner_id)
+        data = ( summoner_profile_icon, summoner_level, summoner_puuid, account_game_name, account_tagLine)
 
-        cursor.execute(update_query, data)
+        return data
+
 
     except requests.exceptions.HTTPError as http_err:
         print(f"HTTP error occurred: {http_err}")
@@ -69,35 +58,11 @@ def update_summoner_info(summoner_id, league_points):
     except Exception as err:
         print(f"Other error occurred: {err}")
         print(f"Response content: {summoner_response.content if 'summoner_response' in locals() else 'No response'}")
-    conn.commit()
-    conn.close()
-
-
-
-# if a dodge has been detected, insert a new entry in the dodge table. Inserts the summonerId, lp lost, the data, the rank, and the lp they were at.
-def insert_dodge_entry(summoner_id, lp_lost, rank, league_points):
-
-    conn = get_db_connection()
-    cursor = conn.cursor()
-    
-    insert_query = """
-        INSERT INTO Dodges (summonerId, lpLost, `rank`, dodgeDate, leaguePoints)
-        VALUES (%s, %s, %s, %s, %s)
-    """
-    data = (summoner_id, lp_lost, rank, datetime.now(), league_points)
-    print(data)
-    cursor.execute(insert_query, data)
-    conn.commit()
-    conn.close()
-
 
 
 # loop that saves their summonerId, league points, games_played, and rank
 # main if else statements to branch out logical paths.
-def update_or_insert_summoner(account, tier):
-
-    conn = get_db_connection()
-    cursor = conn.cursor()
+def insert_summoner(account, tier):
 
     summoner_id = account['summonerId']
     league_points = account['leaguePoints']
@@ -110,44 +75,25 @@ def update_or_insert_summoner(account, tier):
 
     # if else logic path to see what must be done with the result of if the summoner is in the database
     #if the summoner is in the database, check to see if their lp is the same, if it is go next. If their lp is not the same check their games played, if it is different update the database. If the games played is the same, create and execute dodge function to enter dodge info into the database.
-    if result:
-        db_league_points, db_games_played = result
-        # print(summoner_id)
+    if not result:
 
-        if db_games_played == games_played: #there was a game played, update database for rank, lp, and games played
-            if (db_league_points - league_points == 5) or (db_league_points - league_points == 15): 
-                lp_lost = db_league_points - league_points
-                insert_dodge_entry(summoner_id, lp_lost, rank, league_points)
-                update_summoner_info(summoner_id, league_points)
-
-        else: 
-            update_query = """
-            UPDATE Summoner
-            SET leaguePoints = %s, gamesPlayed = %s, `rank` = %s
-            WHERE summonerId = %s
-            """
-            cursor.execute(update_query, (league_points, games_played, rank, summoner_id)) # update database entry for the summonerId
-
-    else:
+        account_data = update_summoner_info(summoner_id)
         insert_query = """
-            INSERT INTO Summoner (summonerId, leaguePoints, gamesPlayed, `rank`)
-            VALUES (%s, %s, %s, %s)
+            INSERT INTO Summoner (summonerId, leaguePoints, gamesPlayed, `rank`, iconId, puuId, gameName, tagLine)
+            VALUES (%s, %s, %s, %s, %s, %s, %s, %s)
         """
         data = (
             summoner_id,
             league_points,
             games_played,
-            rank
-            # account.get('iconId', 23),
-            # account['summonerLevel'],
-            # account['puuId'],
-            # account['gameName'],
-            # account['tagLine']
+            rank,
+            account_data[0],
+            account[1],
+            account_data[2],
+            account_data[3],
+            account_data[4]
         )
         cursor.execute(insert_query, data)
-        conn.commit()
-        conn.close()
-
 
 
 def fetch_challenger_players(api_key):
@@ -169,72 +115,31 @@ def fetch_master_players(api_key):
     return response.json()
 
 
-def main_loop(api_key):
-    while True:
+
+
+
+def populate_NA(api_key):
         try:
             # Fetch and process challenger players
             challenger_players = fetch_challenger_players(api_key)
             for account in challenger_players['entries']:
-                update_or_insert_summoner(account, "challenger")
+                insert_summoner(account, "challenger")
 
             # Fetch and process grandmaster players
             grandmaster_players = fetch_grandmaster_players(api_key)
             for account in grandmaster_players['entries']:
-                update_or_insert_summoner(account, "grandmaster")
+                insert_summoner(account, "grandmaster")
 
             # Fetch and process master players
             master_players = fetch_master_players(api_key)
             for account in master_players['entries']:
-                update_or_insert_summoner(account, "master")
+                insert_summoner(account, "master")
 
         except requests.exceptions.HTTPError as http_err:
             print(f"HTTP error occurred: {http_err}")
         except Exception as err:
             print(f"Other error occurred: {err}")
 
-        # Wait before the next iteration
-        time.sleep(10)
-
-main_loop(api_key)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# # function to get all masters players
-# def parseMasters():
-
-#     getMasters_path = f"https://na1.api.riotgames.com/lol/league/v4/masterleagues/by-queue/RANKED_SOLO_5x5?api_key={api_key}"
-#     masters_players_response = requests.get(getMasters_path)
-#     masters_players = masters_players_response.json()
-#     for account in masters_players['entries']:
-
-#         check_query = "SELECT 1 FROM Summoner WHERE summonerId = %s"
-#         cursor.execute(check_query, (account['summonerId'],))
-#         # Fetch one result
-#         result = cursor.fetchone()
-
-#         if result:
-
-
-#         else:
-#             insert_query = "INSERT INTO SUMMONER (summonerId, leaguePoints, gamesPlayed,`rank`) VALUES (%s, %i, %i, %s)"
-#             cursor.execute(insert_query, (account['summonerId'], account['leaguePoints'], account['gamesPlayed'], account['`rank`']))
-
-# parseMasters()
-# # leaderboard = []
-# # leaderboard.sort(reverse=True)
-# # print(leaderboard)
+populate_NA(api_key)
+conn.commit()
+conn.close()
