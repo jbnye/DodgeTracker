11d568d3acc39de2a5a81d9f66c63d056d207690
Jbnye <jbnye@umich.edu>
2025-04-29 03:02:00 -0400

    added div and fetch for total players in region

--- React/dodge-tracker/src/DodgePage.js
+++ React/dodge-tracker/src/DodgePage.js
@@ -6,6 +6,7 @@ import io from "socket.io-client";
 
 export default function DodgePage() {
   const [socket, setSocket] = useState(null);
+  const [regionTotal, setRegionTotal] = useState(0);
   const { region } = useParams(); // Directly destructure as 'region'
   const upperRegion = region.toUpperCase(); // Will throw if region is undefined
 
@@ -34,6 +35,22 @@ export default function DodgePage() {
     };
   }, []);
 
+  useEffect(() => {
+    const fetchTotalNum = async () => {
+      try {
+        const response = await fetch(
+          `http://127.0.0.1:5000/api/region-total?region=${region}`
+        );
+        const data = await response.json();
+        console.log(data.data);
+        setRegionTotal(data.data);
+      } catch (error) {
+        console.error("Error fetching dodge list:", error);
+      }
+    };
+    fetchTotalNum();
+  }, []);
+
   if (!socket) {
     return (
       <div className="connection-status">Connecting to live updates...</div>
@@ -42,6 +59,17 @@ export default function DodgePage() {
 
   return (
     <div>
+      <div
+        style={{
+          borderBottom: "solid black 3px",
+          borderLeft: "solid black 3px",
+          borderRight: "solid black 3px",
+          justifySelf: "end",
+          padding: "5px",
+        }}
+      >
+        Total Master+ Players in {upperRegion + ": " + regionTotal}
+      </div>
       <div>
         <h1>{upperRegion} Dodge Tracker </h1>
       </div>
